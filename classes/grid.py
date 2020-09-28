60a2314364892ea96ca68280c0e4c4eed6b8f08a
juansuh <juansuhr@gmail.com>
2020-09-28 14:05:13 -0400

    added initial classes

---
+++ classes/grid.py
@@ -0,0 +1,71 @@
+from tile import Tile
+from random import random
+
+class Grid:
+
+  GRID_SIZE = 12
+  STANDABLE_TILE_DENSITY = 0.9
+  WALL_DENSITY = 0.04
+
+  def __init__(self):
+
+    #INDEX WITH [ROW][COL]
+    self._game_map = [[self.__generate_tile(x,y) for x in range(self.GRID_SIZE)] for y in range(self.GRID_SIZE)]
+
+  @property
+  def game_map(self):
+    return self._game_map
+
+  def get_movement(self, row, col, num_moves):
+    tile_list = []
+
+    if(num_moves == 0):
+      return ['(' + str(self._game_map[row][col].row) + ',' + str(self._game_map[row][col].col) + ')']
+    
+    #proceed top if not at border, above tile is standable, and no wall in the way
+    if(row != 0 and self._game_map[row-1][col].standable and not self._game_map[row][col].walls[0] and not self._game_map[row-1][col].walls[2]):
+      tile_list.append(self.get_movement(row-1, col, num_moves-1))
+
+    #proceed left if not at border, left tile is standable, and no wall in the way
+    if(col != 0 and self._game_map[row][col-1].standable and not self._game_map[row][col].walls[1] and not self._game_map[row][col-1].walls[3]):
+      tile_list.append(self.get_movement(row, col-1, num_moves-1))
+
+    #proceed bottom if not at border, lower tile is standable, and no wall in the way
+    if(row != self.GRID_SIZE-1 and self._game_map[row+1][col].standable and not self._game_map[row][col].walls[2] and not self._game_map[row+1][col].walls[0]):
+      tile_list.append(self.get_movement(row+1, col, num_moves-1))
+
+    #proceed right if not at border, right tile is standable, and no wall in the way
+    if(col != self.GRID_SIZE-1 and self._game_map[row][col+1].standable and not self._game_map[row][col].walls[3] and not self._game_map[row][col+1].walls[1]):
+      tile_list.append(self.get_movement(row, col+1, num_moves-1))
+
+    #array syntax flattens and dict.fromKeys removes duplicates
+    return list(dict.fromkeys([item for sublist in tile_list for item in sublist]))
+
+  def print_map_data(self):
+    #Standable map
+    for row in self._game_map:
+      for col in row:
+        print('O' if col.standable else 'X', end = ' ')
+      print()
+
+    #Wall map
+    for row in self._game_map:
+      for col in row:
+        print('|' if col.walls[1] else ' ', end = '')
+        print('=' if col.walls[0] and col.walls[2] else '-' if col.walls[0] else '_' if col.walls[2] else ' ', end = '')
+        print('|' if col.walls[3] else ' ', end = '')
+      print()
+
+    #Position map
+    for row in self._game_map:
+      for col in row:
+        print('(' + str(col.row) + ',' + str(col.col) + ')' , end = ' ')
+      print()
+
+
+  def __generate_tile(self, col, row):
+    walls = [self.__generate_true(self.WALL_DENSITY) for x in range(4)]
+    return Tile(col=col, row=row, standable=self.__generate_true(self.STANDABLE_TILE_DENSITY), walls=walls)
+
+  def __generate_true(self, odds):
+    return True if random() < odds else False
---
+++ classes/tile.py
@@ -0,0 +1,33 @@
+class Tile:
+
+  def __init__(self, col, row, standable, walls):
+
+    self._col = col
+    self._row = row
+    self._standable = standable #bool
+    self._walls = walls #length-4 bool array [top, left, bottom, right]
+    self._occupied = False
+
+  @property
+  def col(self):
+    return self._col
+
+  @property
+  def row(self):
+    return self._row
+
+  @property
+  def standable(self):
+    return self._standable
+
+  @property
+  def walls(self):
+    return self._walls
+
+  @property
+  def occupied(self):
+    return self._occupied
+
+  @occupied.setter
+  def occupied(self, occupied):
+    self._occupied = occupied
