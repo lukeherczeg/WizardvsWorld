573365369de75c173cadeb665c0dfb9ca02af456
lukeherczeg <lukeherczeg@ufl.edu>
2020-10-16 03:22:41 -0400

    Some small edits to draw.py

--- draw.py
+++ draw.py
@@ -72,17 +72,17 @@ def draw_entities(ignorables=None, hard=True):
         SCREEN.blit(entity_img, entity_rect)
         if hard: pygame.display.update(entity_rect)
 
+
 def draw_text(message, size, tile=None, offset=None, color=WHITE):
+    # GOTO provided tile
+    x_pos = tile.get_position().col if tile is not None else 0
+    y_pos = tile.get_position().row if tile is not None else 0
 
-    #GOTO provided tile
-    x_pos = tile.get_position().col if not tile is None else 0
-    y_pos = tile.get_position().row if not tile is None else 0
-
-    #ADD offset to coords
-    x_offset, y_offset = offset if not offset is None else (0,0)
+    # ADD offset to coords
+    x_offset, y_offset = offset if offset is not None else (0, 0)
     x_pos, y_pos = x_pos + x_offset, y_pos + y_offset
 
-    #Draw text
+    # Draw text
     message_font = pygame.font.Font('freesansbold.ttf', size)
     message_text = message_font.render(str(message), True, color)
     message_rect = message_text.get_rect()
@@ -179,27 +179,29 @@ def animate_attack(attacker, victim):
 
 
 def animate_damage(victim, victim_old_hp):
-
     _animate_damage_number(victim, victim_old_hp)
 
     time.sleep(0.2)
 
     _animate_damage_bar(victim, victim_old_hp)
 
-    entity_img = _get_entity_img(entity)
+    entity_img = _get_entity_img(victim)
     wiggle_index = 0
     opacity = 250
 
     while opacity != 0:
         draw_grid()
         draw_entities()
-        _blit_alpha(SCREEN, entity_img,(entity.get_position().col*BLOCK_SIZE + move_wiggle[wiggle_index], entity.get_position().row*BLOCK_SIZE), opacity)
+        _blit_alpha(SCREEN, entity_img, (
+            victim.get_position().col * BLOCK_SIZE + move_wiggle[wiggle_index], victim.get_position().row * BLOCK_SIZE),
+                    opacity)
         pygame.display.flip()
 
         wiggle_index = 0 if wiggle_index == len(move_wiggle) - 1 else wiggle_index + 1
         opacity = opacity - 2
-    
+
     total_refresh_drawing()
+
 
 def _animate_player_attack(coords):
     start_x, start_y, target_x, target_y, x_diff, y_diff, angle = coords
@@ -238,6 +240,7 @@ def _animate_player_attack(coords):
 
     # this is done to re-center the final animation sprite and ensure game state is up to date
     total_refresh_drawing()
+
 
 def _animate_knight_attack():
     draw_grid()
@@ -281,7 +284,7 @@ def _animate_archer_attack(coords):
 def _animate_damage_number(victim, victim_old_hp):
     damage_diff = victim_old_hp - victim.health
 
-    #create number rect
+    # create number rect
     number_font = pygame.font.Font('freesansbold.ttf', 12)
     number_text = number_font.render(str(damage_diff), True, RED)
     number_rect = number_text.get_rect()
@@ -289,31 +292,32 @@ def _animate_damage_number(victim, victim_old_hp):
     number_x_fixed = (victim.get_position().col * BLOCK_SIZE) + 30
     number_rect = number_rect.move([number_x_fixed, number_y_var])
 
-    #create victime rect
+    # create victime rect
     victim_rect = _get_entity_img(victim).get_rect()
-    victim_rect = victim_rect.move([victim.get_position().col*BLOCK_SIZE, victim.get_position().row*BLOCK_SIZE])
+    victim_rect = victim_rect.move([victim.get_position().col * BLOCK_SIZE, victim.get_position().row * BLOCK_SIZE])
 
-    #animation arrays and indexes
-    y_move_amount = [0,0,0,0,0,0,0,0,0,0,-1]
+    # animation arrays and indexes
+    y_move_amount = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1]
     y_move_index = 0
     wiggle_index = 0
     for i in range(120):
-        #animate
+        # animate
         number_rect = number_rect.move([0, y_move_amount[y_move_index]])
         victim_rect = victim_rect.move([move_wiggle[wiggle_index], 0])
 
-        #prepare next frame
+        # prepare next frame
         y_move_index = 0 if y_move_index == len(y_move_amount) - 1 else y_move_index + 1
         wiggle_index = 0 if wiggle_index == len(move_wiggle) - 1 else wiggle_index + 1
 
-        #draw
+        # draw
         draw_grid()
         draw_entities(hard=False, ignorables=[victim])
         SCREEN.blit(number_text, number_rect)
         SCREEN.blit(_get_entity_img(victim), victim_rect)
         pygame.display.flip()
-   
+
     total_refresh_drawing()
+
 
 def _animate_damage_bar(victim, victim_old_hp):
     # HP bar math 4px by 24px
@@ -322,7 +326,7 @@ def _animate_damage_bar(victim, victim_old_hp):
     BAR_LENGTH = 24
     BAR_HEIGHT = 4
 
-    #get ratios for scaling health reduction graphically
+    # get ratios for scaling health reduction graphically
     old_hp_ratio = 0
     new_hp_ratio = 0
     if isinstance(victim, Player):
@@ -335,26 +339,26 @@ def _animate_damage_bar(victim, victim_old_hp):
         new_hp_ratio = victim.health / ARCHER_HEALTH
         old_hp_ratio = victim_old_hp / ARCHER_HEALTH
 
-    #prevent negative health
-    if new_hp_ratio < 0 :
+    # prevent negative health
+    if new_hp_ratio < 0:
         new_hp_ratio = 0
 
-    #track start and stop of green movement
+    # track start and stop of green movement
     green_hp_bar_x_pos = math.floor(BAR_LENGTH * old_hp_ratio)
     green_hp_bar_x_final = math.floor(BAR_LENGTH * new_hp_ratio)
 
-    #animate 
-    x_move_amount = [0,0,0,0,0,0,0,-1]
+    # animate
+    x_move_amount = [0, 0, 0, 0, 0, 0, 0, -1]
     x_move_index = 0
-    while(green_hp_bar_x_pos != green_hp_bar_x_final):
-        #draw
+    while green_hp_bar_x_pos != green_hp_bar_x_final:
+        # draw
         draw_grid()
         draw_entities(hard=False)
         pygame.draw.rect(SCREEN, BRIGHT_RED, (hp_bar_x, hp_bar_y, BAR_LENGTH, BAR_HEIGHT))
         pygame.draw.rect(SCREEN, BRIGHT_GREEN, (hp_bar_x, hp_bar_y, green_hp_bar_x_pos, BAR_HEIGHT))
         pygame.display.flip()
 
-        #load next animation frame
+        # load next animation frame
         green_hp_bar_x_pos = green_hp_bar_x_pos + x_move_amount[x_move_index]
         x_move_index = 0 if x_move_index == len(x_move_amount) - 1 else x_move_index + 1
 
@@ -365,10 +369,10 @@ def _blit_alpha(target, source, location, opacity):
     temp = pygame.Surface((source.get_width(), source.get_height())).convert()
     temp.blit(target, (-x, -y))
     temp.blit(source, (0, 0))
-    temp.set_alpha(opacity)      
+    temp.set_alpha(opacity)
     target.blit(temp, location)
 
-    
+
 def _get_tile_img(tile, tint=None):
     if tile.texture_type == TextureType.GRASS:
         if tint == TileTint.BLUE:
