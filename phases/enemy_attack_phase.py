93e488980f2540227fb23260df0d2151f83438fa
Logan Smith <loganda.smith@ufl.edu>
2020-10-19 00:30:54 -0400

    Build out tutorial for enemies out of range

--- phases/enemy_attack_phase.py
+++ phases/enemy_attack_phase.py
@@ -15,18 +15,23 @@ class EnemyAICombatPhase(Phase):
         self.player_position = self.Player.get_position()
         self.grid = GRID
         self.is_tutorial = True
+        self.attack_tutorial = True
 
     def attack_player_procedure(self, enemy):
         enemy_tiles = GRID.get_attack(self.player_position.row, self.player_position.col, self.Player.range)
         if can_attack(enemy, self.Player):
-            time.sleep(0.25)
+            # TUTORIAL
+            if self.attack_tutorial:
+                MessageBox('Now your enemies will have a chance to attack you!')
+                self.attack_tutorial = False
+                total_refresh_drawing()
 
             perform_attack(enemy, self.Player)
 
             if self.Player.health <= 0:
                 ENTITIES.remove(self.Player)
                 animate_death(self.Player)
-                time.sleep(2)
+                MessageBox('You died. Closing the game... Please relaunch to try again.')
                 pygame.quit()
                 sys.exit()
             elif self.Player.health > 0:
@@ -34,22 +39,27 @@ class EnemyAICombatPhase(Phase):
                 attacker = CounterAttack(self.Player, enemy, enemy_tiles)
                 attacker.attempt_counter_attack()
 
+            return True
+        else:
+            return False
+
     def enter(self):
         self.Enemies = ENTITIES[1:]
         #draw_text_abs('Enemy Attack', 72, WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2)
         #pygame.time.delay(2000)
         self.player_position = self.Player.currentTile
 
-        # TUTORIAL
-        if self.is_tutorial:
-            MessageBox('Now your enemies will have a chance to attack you!')
-            total_refresh_drawing()
-
     def update(self):
         print('Entering Enemy Attack Computation')
+        valid_attackers = 0
         for enemy in self.Enemies:
             if isinstance(enemy, Enemy):
-                self.attack_player_procedure(enemy)
+                if self.attack_player_procedure(enemy):
+                    valid_attackers += 1
+        # TUTORIAL
+        if valid_attackers == 0 and self.attack_tutorial:
+            MessageBox('Now your enemies get the chance to attack you! Fortunately for you, none are in range!')
+            total_refresh_drawing()
 
     def exit(self):
         print('Exiting Enemy Phase...')
