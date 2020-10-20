3a3f57aee2d7dfbbc1265a722a10a30151dfed11
Logan Smith <loganda.smith@ufl.edu>
2020-10-20 02:44:38 -0400

    Update Version Number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme:
 
 setuptools.setup(
     name="WizardVsWorld",
-    version= "0.1.3",
+    version= "0.1.4",
     author="Logan D.G. Smith",
     author_email="loganda.smith@ufl.edu",
     description="2D tactical game involving magic and malice.",
