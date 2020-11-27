b787af2a1041feb74fac8899fe4a73cf4d3911e2
juansuh <juansuhr@gmail.com>
2020-11-27 18:34:24 -0500

    imported attack sounds

--- WizardVsWorld/assets/sounds/sound_loader.py
+++ WizardVsWorld/assets/sounds/sound_loader.py
@@ -5,4 +5,6 @@ pygame.mixer.init()
 
 asset_path = os.path.dirname(__file__)
 
-menu_music = pygame.mixer.Sound(os.path.join(asset_path, 'menu_song.wav'))
+menu_music = pygame.mixer.Sound(os.path.join(asset_path, 'menu_music.wav'))
+fireball_attack = pygame.mixer.Sound(os.path.join(asset_path, 'fireball_attack.wav'))
+sword_attack = pygame.mixer.Sound(os.path.join(asset_path, 'sword_attack.wav'))
