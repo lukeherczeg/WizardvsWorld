965208293cbafed4296d044211749b718b62a42d
Gus Segovia <40403478+gussegovia98@users.noreply.github.com>
2020-11-30 01:17:12 -0500

    Updated archers and knights scaling values

--- WizardVsWorld/classes/entity.py
+++ WizardVsWorld/classes/entity.py
@@ -200,10 +200,10 @@ class Knight(Enemy):
     def __init__(self, level):
         super().__init__()
         self.max_movement = 3
-        self.health = 50 + (level * 5)
+        self.health = 55 + (level * 6)
         self.max_health = self.health
-        self.attack = 15 + (level * 2)
-        self.defense = 5 + (level * 1)
+        self.attack = 20 + (level * 3)
+        self.defense = 5 + (level * 5)
         self.crit_chance = 5
         self.range = 1
 
@@ -215,10 +215,10 @@ class Archer(Enemy):
     def __init__(self, level):
         super().__init__()
         self.max_movement = 4
-        self.health = 30 + (level * 2)
+        self.health = 40 + (level * 4)
         self.max_health = self.health
-        self.attack = 10 + (level * 3)
-        self.defense = 0 + (level * 1)
+        self.attack = 15 + (level * 4)
+        self.defense = 0 + (level * 2)
         self.crit_chance = 15
         self.range = 2
 
