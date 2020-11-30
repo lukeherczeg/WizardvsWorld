3824270423e7f7bac24b8fc65e1723ecb8713bc6
juansuh <juansuhr@gmail.com>
2020-11-29 20:54:38 -0500

    adjusted sound values

--- WizardVsWorld/assets/sounds/sound_loader.py
+++ WizardVsWorld/assets/sounds/sound_loader.py
@@ -8,10 +8,11 @@ asset_path = os.path.dirname(__file__)
 
 menu_music = pygame.mixer.Sound(os.path.join(asset_path, 'menu_music.ogg'))
 game_music = pygame.mixer.Sound(os.path.join(asset_path, 'game_music.ogg'))
-game_music.set_volume(0.3)
+game_music.set_volume(0.15)
 
 fireball_attack_sound = pygame.mixer.Sound(os.path.join(asset_path, 'fireball_attack.ogg'))
 sword_attack_sound = pygame.mixer.Sound(os.path.join(asset_path, 'sword_attack.ogg'))
 arrow_attack_sound = pygame.mixer.Sound(os.path.join(asset_path, 'arrow_attack.ogg'))
 
 death_sound = pygame.mixer.Sound(os.path.join(asset_path, 'enemy_died.ogg'))
+death_sound.set_volume(0.3)
