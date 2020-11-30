de4fc77252bdb77c3ff075a0331e30ce7fe5efce
juansuh <juansuhr@gmail.com>
2020-11-29 19:31:54 -0500

    change healing time

--- WizardVsWorld/classes/entity.py
+++ WizardVsWorld/classes/entity.py
@@ -89,7 +89,6 @@ class Player(Entity):
         return "The Wizard"
 
     def heal(self, target):
-        self.healing = True
         target.health = target.max_health
 
     def refresh_spells(self):
